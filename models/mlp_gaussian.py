ebed41acd627a13aac83ad2fe81dafc142361c36
Jean Bernard Hayet <jbhayet@cimat.mx>
2023-03-28 09:05:25 -0600

    Changing the optimizer leads to better results

--- models/mlp_gaussian.py
+++ models/mlp_gaussian.py
@@ -6,13 +6,13 @@ class MLP_gaussian1(nn.Module):
         super().__init__()
         # Layers
         self.fc1 = nn.Linear(input_size, hidden_dim)
-        self.relu1 = nn.ReLU()
+        self.activation = nn.Tanh()
         self.fc2 = nn.Linear(hidden_dim, output_size)
 
     def forward(self, x):
         '''Forward pass'''
         out = self.fc1(x)
-        out = self.relu1(out)
+        out = self.activation(out)
         out = self.fc2(out)
         return out
 
@@ -20,21 +20,13 @@ class MLP_gaussian2(nn.Module):
     def __init__(self, input_size, hidden_dim, output_size):
         super().__init__()
         # Layers
+        self.activation = nn.ReLU()
         self.fc1 = nn.Linear(input_size, hidden_dim)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.relu2 = nn.ReLU()
-        self.fc3 = nn.Linear(hidden_dim, hidden_dim)
-        self.relu3 = nn.ReLU()
-        self.fc4 = nn.Linear(hidden_dim, output_size)
+        self.fc2 = nn.Linear(hidden_dim, output_size)
 
     def forward(self, x):
         '''Forward pass'''
         out = self.fc1(x)
-        out = self.relu1(out)
+        out = self.activation(out)
         out = self.fc2(out)
-        out = self.relu2(out)
-        out = self.fc3(out)
-        out = self.relu3(out)
-        out = self.fc4(out)
         return out
