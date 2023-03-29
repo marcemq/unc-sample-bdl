f8687cc80c4c813a246321be0eb0edfa4d328fbe
Morales Quispe, Marcela <marcela.morales.quispe@gmail.com>
2023-03-28 23:26:12 -0600

    Generated data in range 1-1.5
    Signed-off-by: Morales Quispe, Marcela <marcela.morales.quispe@gmail.com>

--- data/generateData.py
+++ data/generateData.py
@@ -10,15 +10,26 @@ def f(samples=100):
     y = x + 0.3*np.sin(2*np.pi*(x+e)) + 0.3*np.sin(4*np.pi*(x+e)) + e
     return x, y
 
+def fNew(samples=100):
+    mu, sigma = 0, 0.002
+    e = np.random.normal(mu, sigma, samples).reshape(samples, 1)
+    x = np.linspace(1, 1.5, samples).reshape(samples,1)
+    y = x + 0.3*np.sin(2*np.pi*(x+e)) + 0.3*np.sin(4*np.pi*(x+e)) + e
+    return x, y
+
 def saveData(x, y, fileName="data.csv"):
     data = np.concatenate([x,y], axis=1)
     np.savetxt(fileName, data, delimiter=',', header="x,y")
 
 if __name__ == "__main__":
     x, y = f(samples=200)
-    plt.scatter(x, y, marker ='x', color='blue', s=10)
+    xUnseen, yUnseen = fNew()
+    plt.scatter(x, y, marker ='x', color='b', s=10)
+    plt.scatter(xUnseen, yUnseen, marker ='x', color='c', s=10)
     plt.xlabel('x')
     plt.ylabel('f(x)')
     plt.title(r'$f(x) = x + 0.3\sin(2\pi(x+e)) + 0.3\sin(4\pi(x+e)) + e $')
     plt.show()
+
     saveData(x,y)
+    saveData(xUnseen,yUnseen, "dataUnseen.csv")
